bd69c55cdf2f23763f41c44fc24114a076657a25
Subhash Adhikari <subhash@Subhashs-MacBook-Air.local>
2025-06-17 14:43:10 +0545

    feat: initial React + TypeScript + Tailwind setup in frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-grey-100">
+      <h1 className="text-4xl font-bold text-indigo-600">
+        Tailwind v3 is working! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
